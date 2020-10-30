3597d87a6cfd39638a094c4d6da9cd064c89bbc3
Tochukwu Ozurumba <ozurumbatochukwu@yahoo.com>
2020-10-31 00:16:55 +0100

    :star: get categories added

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,7 +1,9 @@
 import AuthController from './authController';
 import SupplierController from './supplierController';
+import CategoryController from './categoryController';
 
 export {
   AuthController,
   SupplierController,
+  CategoryController
 };

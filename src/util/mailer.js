cd199207af72b1899f9fbf992c5483a686c10f97
Tochukwu Ozurumba <ozurumbatochukwu@yahoo.com>
2020-11-13 22:58:16 +0100

    :octocat: mailer is clickable

--- src/util/mailer.js
+++ src/util/mailer.js
@@ -32,7 +32,18 @@ const Mailer = {
       to: vendor ? vendor.companyEmail : email,
       from: ADMIN_EMAIL,
       subject: 'RESET PASSWORD',
-      html: `<p>Please click on this link to reset your password, ${passwordResetLink}</p>`
+      html: `<h4>You've requested for a password reset. Click the link below to reset your password successfully!</h4><br><br>
+      <a href="${passwordResetLink}" target="_blank" style="background-color: #B11F24;
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;">Reset Password</a>
+      `
     };
     try {
       await sendgrid.send(mail);

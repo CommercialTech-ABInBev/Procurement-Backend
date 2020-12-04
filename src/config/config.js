2090d5dd98b71b3ca5abb327ff0370ae87827d9a
Tochukwu Ozurumba <ozurumbatochukwu@yahoo.com>
2020-12-04 17:55:46 +0100

    :octocat: production database testing again

--- src/config/config.js
+++ src/config/config.js
@@ -1,6 +1,16 @@
 import env from './env';
+// import mysql from './mysql2';
 
 module.exports = {
+  production: {
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    dialect: 'mysql',
+    ssl: true
+  },
+
   development: {
     url: env.DATABASE_URL_DEV || env.LOCAL_URL,
     dialect: 'mysql',

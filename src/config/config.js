81f5cc0f8f81eede3e80e7f1b4b371895ffe1800
Tochukwu Ozurumba <ozurumbatochukwu@yahoo.com>
2021-02-08 19:42:56 +0100

    :octocat: config refactor

--- src/config/config.js
+++ src/config/config.js
@@ -16,17 +16,19 @@ module.exports = {
      }
   },
   development: {
-    host: process.env.DB_HOST,
-    database: process.env.DB_NAME,
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    dialect: 'mysql',
-    ssl: true,
-    dialectOptions: {
-       ssl: {
-          require: true
-       }
-     }
+    url: env.DATABASE_URL_DEV || env.LOCAL_URL,
+    dialect: 'mysql'
+    // host: process.env.DB_HOST,
+    // database: process.env.DB_NAME,
+    // username: process.env.DB_USER,
+    // password: process.env.DB_PASSWORD,
+    // dialect: 'mysql',
+    // ssl: true,
+    // dialectOptions: {
+    //    ssl: {
+    //       require: true
+    //    }
+    //  }
   },
 
   test: {
